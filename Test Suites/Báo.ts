9eef71d231ea8aca870de946e78c0d9064701d8e
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-02-24 11:01:29 +0700

    Update wrong content

--- Test Suites/Báo.ts
+++ Test Suites/Báo.ts
@@ -10,8 +10,7 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>54ab0d9b-fe22-4b13-9eb8-75d9196f0773</testSuiteGuid>
-   #Wrong line
+   <testSuiteGuid>54ab0d9b-fe22-4b13-9eb8-75d9196f0773</testSuite#Wrong lineGuid>
    <testCaseLink>
       <guid>2c2fc12d-eb5d-4b06-b962-01aded5d0976</guid>
       <isReuseDriver>false</isReuseDriver>
